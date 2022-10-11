53a4f99247c2ae55ac078324961bc31464c79be2
mevd3 <mevd3@DESKTOP-QRSMTV5>
2022-10-11 21:29:11 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Тип {0,8} |\t.NET {1,8} |\tРАзмер = {2} \t| MIN = {3,30}|\t| MAX = {4}";
+
+            Console.Title = "Типы данных C#";
+
+            Console.WriteLine(string.Format(msg, "byte", typeof(byte).Name, sizeof(byte), byte.MinValue, byte.MaxValue));
+            Console.WriteLine(string.Format(msg, "sbyte", typeof(sbyte).Name, sizeof(sbyte), sbyte.MinValue, sbyte.MaxValue));
+            Console.WriteLine(string.Format(msg, "short", typeof(short).Name, sizeof(short), short.MinValue, short.MaxValue));
+            Console.WriteLine(string.Format(msg, "int", typeof(int).Name, sizeof(int), int.MinValue, int.MaxValue));
+            Console.WriteLine(string.Format(msg, "float", typeof(float).Name, sizeof(float), float.MinValue, float.MaxValue));
+            Console.WriteLine(string.Format(msg, "char", typeof(char).Name, sizeof(char), char.MinValue, char.MaxValue));
+            //Console.WriteLine(string.Format(msg, "byte", typeof(string).Name, sizeof(string), string.MinValue, string.MaxValue));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int b = 9;
+
+            int c,d;
+
+            c = 65;
+            d = 99;
+
+            bool variable = false;
+            bool Variable = true;
+
+            double doub = 1.33;
+
+            char letter = 'a';
+            string stroka = "mnogo bukv";
+
+            int a;
+            a = 5;
+
+            Console.WriteLine(a);
+
+            a = 66;
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+
+        }
+    }
+}
