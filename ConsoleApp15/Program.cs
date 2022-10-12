93e15e933eb5becfcf08b51e1247a174a6970178
Михаил Евдокимов <mevd3685@gmail.com>
2022-10-12 17:22:36 +0300

    Сделаны 20 и 21 урок, работап на 12.10 закончена

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (int i =0; i<10; i++)
+            {
+                string msg = Console.ReadLine();
+                if (msg == "exit")
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }*/
+
+            for (int i =0; i < 5; i++)
+            {
+                if (i == 2)
+                {
+                    continue;//чтобы пропустить какую то итерацию, но не завершать цикл
+                }
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
