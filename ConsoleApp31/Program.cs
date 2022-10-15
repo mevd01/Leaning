a5a3d6ca263aa76569ba3f95bca2616d2f654dc0
Михаил Евдокимов <mevd3685@gmail.com>
2022-10-15 13:29:10 +0300

    Сделаны уроки до 40 включительно

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        class MyClass
+        {
+
+        }
+
+        struct MyStruct
+        {
+
+        }
+
+        static void Foo()
+        {
+            int b;//удалиться после выхода из метода
+            int[] a = new int[10];//ссылка будет удалена, но данные остануться, их уберет сборщик мусора
+        }
+
+        static void Main(string[] args)
+        {
+            bool b;//false
+            int a;//0
+            double d;//0
+            Random ramdon;//null
+            int[] arr;//null
+            MyClass myClass;//null
+            MyStruct myStruct;
+            //null - пустая ссылка
+            int[] arra = new int[10];//0000000000
+            arra = null; // разорвали связь, сборщик мусора очистит ура ура
+
+
+        }
+    }
+}
---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+
+            /*if (str == null)
+            {
+                Console.WriteLine("нет данных");
+            }
+            else
+            {
+                Console.WriteLine(str);
+            }*/
+
+            Console.WriteLine(str ?? "No data");//?? проверка на null
+
+            string result = str ?? string.Empty;// если пусто то что вписать??
+
+            Console.WriteLine("Количество символов в строке " + result.Length);
+
+        }
+    }
+}
